f2459d3daf2ca05d7361d5a74e959ee7d4ec788c
konexio <fatima.b_1998@outlook.fr>
2020-12-08 23:01:00 +0100

    suppression fonction sign in , ajout de la fonction login

--- back-end/controllers/authentification/auth.js
+++ back-end/controllers/authentification/auth.js
@@ -1,41 +1,37 @@
-const User = require ("../../schema/adminSchema.js");
-const passwordHash = require ("password-hash");
+const Admin = require("../../schema/adminSchema.js");
+const passwordHash = require("password-hash");
 
-async function signup (req, res) {
-    const {passeword, email} = req.body;
+async function login(req, res) {
+    const { password, email } = req.body;
     if (!email || !password) {
         //si absence ou invalidité de mail ou password
-        return res.status(400).json ({
-            text : "Demande invalide"
+        return res.status(400).json({
+            text: "Demande invalide"
         });
     }
-// création obijet admin + hash mdp
-const admin = {
-    email,
-    password : passwordHash.generate(password)
-};
-// on verifie si l'utilisateur existe deja dans la DB
-try {
-    const findUser = await admin.findOne ({
-        email
-    });
-    if (findUser) {
-        return res.status(400).json ({
-            text : "cet utilisateur existe déjà"
-        })
+    // on verifie si l'utilisateur existe dans la DB
+    try {
+        const findUser = await Admin.findOne({ email });
+
+        if (!findUser)
+            return res.status(400).json({
+                text: "identifiant incorrect"
+            });
+
+        if (!findUser.authenticate(password))
+            return res.status(401).json({
+                text: "mot de passe incorrect"
+            });
+
+
+        return res.status(200).json({
+            token: findUser.getToken(),
+            text: "Authentification OK",
+        });
+
+    } catch (error) {
+        return res.status(500).json({ error });
+
     }
-} catch (error) {
-    return res.status(500).json({ error});
 }
-try {
-//enregistrement de l'utilisateur en base de données
-const userData = new Admin(user);
-const userObject = await userData.save();
-return res.tatus (200).json ({
-    text: "Connexion OK",
-    token: userObject.getToken()
-});
-} catch (error) {
-    return res.status(500).json({error});
-}
-}
+
