0dff20a5db85794ff75c25987cc6c7d622fb583a
SarahEDD <sarah.eddaira@gmail.com>
2020-12-14 13:35:56 +0100

    MàJ

--- back-end/controllers/creation.js
+++ back-end/controllers/creation.js
@@ -2,31 +2,32 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads')
-    },
-    filename: function (req, file, cb) {
-        const { username } = req.body;
-        // username_data
-        const date = new Date();
-        const dateYYYYMMDD = date.toISOString().substring(0, 10);
-        console.log('dateYYYYMMDD', dateYYYYMMDD);
-        const extension = file.originalname.split('.').pop();
-        const filename = `${username}_${dateYYYYMMDD}.${extension}`;
-        cb(null, filename);
-    }
-})
 
-const upload = multer({ storage: storage });
+const uploader = require ('../config/fileUploader.js');
 
 
 module.exports = function (passport, Posts) {
 
-
-    router.post('/creation', upload.single('image'), async (req, res) => {
+    router.get('/admin/liste', async (req, res, next) => {
         try {
+            const posts = await Posts.find({}).lean().exec()
+            console.log(liste)
+            res.render('admin/liste', {
+                posts,
+                isAuthenticated: req.isAuthenticated(),
+                username: req.isAuthenticated() ? req.user.username: null
+            })
+        } catch(err) {
+            res.status(500).send(err)
+        }
+        
+    })
+
+
+    router.post('/admin/creation', uploader.single('image'), async (req, res) => {
+        try { 
             const resultat = await Posts.create({
+                type: req.body.type,
                 titre: req.body.titre,
                 description: req.body.description,
                 texte: req.body.texte,
@@ -36,32 +37,6 @@ module.exports = function (passport, Posts) {
             console.log(req.file)
         } catch (err) {
             res.status(500).send(err);
+
         }
     });
-
-
-
-
-
-    // Récupération des posts (articles) dans la db
-    router.get('/admin/liste', (req, res, next) => {
-        postModel.find()
-            .then(posts => {
-                res.render('admin/liste', {
-                    posts
-                })
-            })
-            .catch(dbErr => next(dbErr));
-    });
-
-    // Récupération des posts à modifier
-    router.get('/admin/liste/modification/:id', (req, res, next) => {
-        postModel.findById(req.param.id)
-            .then(post => res.render("/admin/creation/modification", {
-                posts
-            }))
-            .catch(dbErr => next(dbErr));
-    });
-
-    return router
-};
