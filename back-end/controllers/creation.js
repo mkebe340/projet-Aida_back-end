8b66ae15a9d8547986614eaf610f3a7cad96a855
SarahEDD <sarah.eddaira@gmail.com>
2020-12-14 17:08:29 +0100

    modif localhost en 3001

--- back-end/controllers/creation.js
+++ back-end/controllers/creation.js
@@ -9,15 +9,18 @@ const uploader = require ('../config/fileUploader.js');
 module.exports = function (passport, Posts) {
 
     router.get('/admin/liste', async (req, res, next) => {
+        console.log("TEST admin liste")
         try {
             const posts = await Posts.find({}).lean().exec()
-            console.log(liste)
+            console.log("liste")
             res.render('admin/liste', {
                 posts,
                 isAuthenticated: req.isAuthenticated(),
                 username: req.isAuthenticated() ? req.user.username: null
             })
         } catch(err) {
+            console.log("error :", err)
+
             res.status(500).send(err)
         }
         
@@ -31,7 +34,7 @@ module.exports = function (passport, Posts) {
                 titre: req.body.titre,
                 description: req.body.description,
                 texte: req.body.texte,
-                imageUrl: `http://localhost:3000/uploads/${req.file.filename}`
+                imageUrl: `http://localhost:3001/uploads/${req.file.filename}`
             }).exec()
             res.send(resultat);
             console.log(req.file)
@@ -41,29 +44,6 @@ module.exports = function (passport, Posts) {
         }
     });
 
-
-
-
-
-    // Récupération des posts (articles) dans la db
-    router.get('/admin/liste', (req, res, next) => {
-        postModel.find()
-            .then(posts => {
-                res.render('admin/liste', {
-                    posts
-                })
-            })
-            .catch(dbErr => next(dbErr));
-    });
-
-    // Récupération des posts à modifier
-    router.get('/admin/liste/modification/:id', (req, res, next) => {
-        postModel.findById(req.param.id)
-            .then(post => res.render("/admin/creation/modification", {
-                posts
-            }))
-            .catch(dbErr => next(dbErr));
-    });
 
     return router
 };
